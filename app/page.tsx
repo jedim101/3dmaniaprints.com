824df07d84773790b87a0ec3d4a38a2453781464
matthewglasser <matthewsglasser@gmail.com>
2024-06-27 11:03:32 -0400

    update spline model

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Spline from "@splinetool/react-spline";
 export default function Home() {
 	return (
 		<div className="p-24">
-			<div className="flex h-screen w-full">
+			<div className="h-screen w-full">
 				<Spline scene="https://prod.spline.design/yCqlyv1GCEbsBlFx/scene.splinecode" />
-				<Spline scene="https://prod.spline.design/9Zy5vZs9Kiit-6pL/scene.splinecode" />
+				{/* <Spline scene="https://prod.spline.design/9Zy5vZs9Kiit-6pL/scene.splinecode" /> */}
 			</div>
 		</div>
 	);
