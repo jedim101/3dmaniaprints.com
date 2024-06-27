33c568ffb7f72e95e46f0d683894f5ca6ff31cec
matthewglasser <matthewsglasser@gmail.com>
2024-06-27 18:33:16 -0400

    change tooltip text

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 			<Spline scene="https://prod.spline.design/yCqlyv1GCEbsBlFx/scene.splinecode" />
 			<div
 				className={`absolute bottom-12 left-1/2 flex -translate-x-1/2 items-center space-x-1 rounded-full bg-gray-700 px-5 py-3 ${!tooltip ? "hidden" : ""}`}>
-				<span>Click and drag the objects!</span>
+				<span>Click the objects to learn more!</span>
 				<button type="button" onClick={() => setTooltip(false)}>
 					<XMarkIcon className="h-4 w-4" />
 				</button>
