7f6daeb64454a1aee1af6224e1782f53905c627f
matthewglasser <matthewsglasser@gmail.com>
2024-06-27 08:29:05 -0400

    finish header (for now)

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -3,9 +3,10 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<div className="flex w-full items-center bg-slate-900 p-4">
-			<Link href="/" className="h-12 w-12">
-				<Image src="/images/3D Mania.png" className="h-full w-full object-contain" height={48} width={48} alt="logo" />
+		<div className="flex w-full items-center bg-slate-800 p-2">
+			<Link href="/" className="flex items-center space-x-2">
+				<Image src="/images/3D Mania.png" className="h-12 w-12 object-contain" height={48} width={48} alt="logo" />
+				<span className="translate-y-0.5 text-xl font-bold text-slate-200">3D Mania Prints</span>
 			</Link>
 		</div>
 	);
