52436f18d927c70ef9983daf7b132153636669b1
matthewglasser <matthewsglasser@gmail.com>
2024-06-28 11:38:17 -0400

    products page

--- app/(pages)/products/page.tsx
+++ app/(pages)/products/page.tsx
@@ -6,13 +6,14 @@ export default function Products() {
 	return (
 		<div className="flex flex-col items-center space-y-24 p-12 pt-20">
 			<h1 className="w-full text-center text-7xl font-semibold">Products</h1>
-			<div className="mx-auto grid w-full max-w-4xl grid-cols-2 gap-12">
+			<div className="mx-auto grid w-fit grid-cols-1 gap-12 lg:grid-cols-2 xl:grid-cols-3">
 				{products.map((product) => (
 					<Link
 						key={product.name}
 						href={`/products/${product.name.replace(" ", "_")}`}
-						className="flex w-full rounded-xl bg-slate-700">
+						className="flex w-full max-w-sm flex-col space-y-6 rounded-3xl bg-slate-700 p-8 pt-4 text-center duration-150 hover:scale-110">
 						<Spline className="aspect-square w-full" scene={product.spline} />
+						<span className="text-3xl">{product.name}</span>
 					</Link>
 				))}
 			</div>
