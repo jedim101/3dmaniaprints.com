81b8f8d789ec7a01600bed4a54857e891abaff9d
matthewglasser <matthewsglasser@gmail.com>
2024-06-27 22:44:41 -0400

    added purchase button

--- app/(pages)/products/[product]/page.tsx
+++ app/(pages)/products/[product]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { Product } from "@/types";
+import Link from "next/link";
 
 const product = {
 	name: "Product Name",
@@ -15,10 +16,15 @@ export default function Home() {
 	return (
 		<div className="flex space-x-12 p-12">
 			<div className="h-full w-full bg-slate-700 p-12 text-center text-5xl">Spline Here</div>
-			<div className="h-full w-2/3 rounded-3xl border border-slate-700 p-8">
+			<div className="flex h-full w-2/3 flex-col rounded-3xl border border-slate-700 p-8">
 				<h1 className="text-3xl font-semibold">{product.name}</h1>
 				<p className="mt-4 font-light text-slate-400">{product.description}</p>
 				<div className="mt-6 text-lg">${product.price.toFixed(2)}</div>
+				<Link
+					href={product.etsy}
+					className="mt-10 w-full rounded-full bg-indigo-600 p-3 text-center font-bold hover:bg-indigo-500">
+					Purchase on Etsy
+				</Link>
 			</div>
 		</div>
 	);
